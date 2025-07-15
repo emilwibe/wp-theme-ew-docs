3845b1ff975a1f87fe03602259638b81a387e1a5
Emil Wibe <mail@emilwibe.dk>
2025-07-15 17:32:29 +0200

    [bulk upload]

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php get_header(); ?>
 
 <main class="l-wrapper xl">
-    
+
+    <nav class="nav-primary">
+
+    </nav>
+
+    <div class="content">
+
+    </div><!--/.content-->
+
 </main>
 
 <?php get_footer(); ?>
