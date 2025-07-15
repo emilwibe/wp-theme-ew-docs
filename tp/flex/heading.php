3c21af19e767f94da7c7151e205e637723505bae
Emil Wibe <mail@emilwibe.dk>
2025-07-15 20:04:06 +0200

    [bulk upload]

--- tp/flex/heading.php
+++ tp/flex/heading.php
@@ -2,6 +2,6 @@
 
 <section class="l-wrapper xl flex-heading">
 
-    <h2><?php echo get_sub_field('flex_heading_heading'); ?> <a href="#top-anchor">#tiltoppen</a></h2>
+    <h2><?php echo get_sub_field('flex_heading_heading'); ?> <a href="#top-anchor" class="scroll-to-top-link">#tiltoppen</a></h2>
 
 </section><!--/.flex-heading-->
