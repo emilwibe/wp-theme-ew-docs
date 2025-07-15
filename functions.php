7b590c0733c1b9d4dd1c5e84369e18366a1965bc
Emil Wibe <mail@emilwibe.dk>
2025-07-15 20:12:00 +0200

    [bulk upload]

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_theme_support('title-tag');
 
 function ew_enqueues() {
     wp_enqueue_style( 'ew-core-styles', get_stylesheet_uri() . "?time=" . time());
-    wp_enqueue_scripts('ew-core-scripts', get_template_directory_uri() . '/scripts-footer.js', '', '', true);
+    wp_enqueue_script('ew-core-scripts', get_template_directory_uri() . '/scripts-footer.js', '', '', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'ew_enqueues' );
